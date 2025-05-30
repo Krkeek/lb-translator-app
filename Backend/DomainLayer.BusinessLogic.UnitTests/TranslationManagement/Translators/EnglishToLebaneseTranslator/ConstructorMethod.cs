1453f7c93e75b618bfa6235af336bbb959206949
Ahmad <ahmadhijazi_me@hotmail.com>
2025-05-31 00:00:53 +0200

    Fix Pipeline

--- Backend/DomainLayer.BusinessLogic.UnitTests/TranslationManagement/Translators/EnglishToLebaneseTranslator/ConstructorMethod.cs
+++ Backend/DomainLayer.BusinessLogic.UnitTests/TranslationManagement/Translators/EnglishToLebaneseTranslator/ConstructorMethod.cs
@@ -5,16 +5,14 @@ using DomainLayer.BusinessLogic.TranslationManagement.Translators;
 
 public class ConstructorMethod
 {
-    private EnglishToLebaneseTranslator _translator;
-
     [Fact]
     public void ConstructorShouldSetCorrectLanguages()
     {
         // Act
-        this._translator = new EnglishToLebaneseTranslator();
+        var translator = new EnglishToLebaneseTranslator();
 
         // Assert
-        Assert.Equal(LanguageType.English, this._translator.SourceLanguage);
-        Assert.Equal(LanguageType.Lebanese, this._translator.TargetLanguage);
+        Assert.Equal(LanguageType.English, translator.SourceLanguage);
+        Assert.Equal(LanguageType.Lebanese, translator.TargetLanguage);
     }
 }
